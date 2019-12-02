a10f4665819f0b6d2fddf9e5e73a00b1eb407398
Timothy H <timh1203@gmail.com>
2019-12-02 10:34:28 -0600

    Add StatefulHello component

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import Hello from './components/Hello';
+import StatefulHello from './components/StatefulHello';
 
 ReactDOM.render(
-  <Hello name="TypeScript" enthusiasmLevel={10} />,
+  <StatefulHello name="TypeScript" enthusiasmLevel={10} />,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
