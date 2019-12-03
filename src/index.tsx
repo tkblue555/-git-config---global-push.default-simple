e97f08e4c95f4672dc875dda256f1a5c2cbf2f80
Timothy H <timh1203@gmail.com>
2019-12-03 11:40:23 -0600

    Creating a store

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,25 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
-import StatefulHello from './components/StatefulHello';
+// PACKAGE IMPORTS
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+// LOCAL IMPORTS
+import './index.css'
+import registerServiceWorker from './registerServiceWorker'
+import Hello from './containers/Hello'
+import { enthusiasm } from './reducers/index'
+import { StoreState } from './types/index'
+
+const store = createStore<StoreState>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+})
 
 ReactDOM.render(
-  <StatefulHello name="TypeScript" enthusiasmLevel={10} />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById('root') as HTMLElement
-);
-registerServiceWorker();
+)
+registerServiceWorker()
